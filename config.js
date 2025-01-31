c5ebaa4ceb0d5f9e758da17cbf566103eb264fd0
Orytuna <115263561+Orytuna@users.noreply.github.com>
2025-01-31 01:36:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  levanter_31a8f7e4020b564ea9835c19ab1afbeff5: (process.env.SESSION_ID || '').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
